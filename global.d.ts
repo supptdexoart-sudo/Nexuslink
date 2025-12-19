9ec589876dfad4c48352cdc4eb849eb22d4476bf
supptdexoart-sudo <supptdexoart@gmail.com>
2025-12-19 15:02:10 +0100

    Delete global.d.ts (#5)

--- global.d.ts
+++
@@ -1,9 +0,0 @@
-
-// Augmentace namespace NodeJS pro správnou typovou kontrolu process.env v rámci celé aplikace
-declare namespace NodeJS {
-  interface ProcessEnv {
-    API_KEY: string;
-    NODE_ENV: 'development' | 'production' | 'test';
-    [key: string]: string | undefined;
-  }
-}
