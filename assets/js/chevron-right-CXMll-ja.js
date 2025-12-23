e02cb81c7c51e1316c62db70c3a95ff289cb02dd
zbynek <tvuj.email@example.com>
2025-12-23 10:38:16 +0100

    Updates

--- assets/js/chevron-right-CXMll-ja.js
+++ assets/js/chevron-right-CXMll-ja.js
@@ -1,4 +1,4 @@
-import{c as t}from"./index-DgRZv62J.js";/**
+import{c as t}from"./index-Dwzf6489.js";/**
  * @license lucide-react v0.395.0 - ISC
  *
  * This source code is licensed under the ISC license.
